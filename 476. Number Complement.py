84ea8f014919b36966bff13ea57d1ec60ba4c92f
nickzhq <Nick@Houqis-MacBook-Air.local>
2017-05-06 16:45:45 +0800

    476 Number Complement

---
+++ 476. Number Complement.py
@@ -0,0 +1,12 @@
+# 476. Number Complement.py
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        i = 1
+        while i <= num:
+            i = i << 1
+            
+        return (i-1)^num
